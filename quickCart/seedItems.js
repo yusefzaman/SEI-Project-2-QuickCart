233b7a70c50280790563a514b07db8d8c9c038f0
zainabtalaq <zainab.altallaq@gmail.com>
2024-05-24 21:23:45 +0300

    create show category page and filter it

--- quickCart/seedItems.js
+++ quickCart/seedItems.js
@@ -5,12 +5,22 @@ const Item = require("./models/item")
 
 const createItem = async () => {
   try {
-    const doc = await Item.create({
-      itemName: "T-Shirt",
-      itemImg: "String",
-      itemPrice: 15,
-      itemDescription: "String",
-    })
+    const doc = await Item.create(
+      {
+        itemName: "T-Shirt",
+        itemImg: "String",
+        itemPrice: 15,
+        itemDescription: "String",
+        itemType: "Clothes",
+      },
+      {
+        itemName: "sjkdhsh",
+        itemImg: "String",
+        itemPrice: 15,
+        itemDescription: "String",
+        itemType: "Furniture",
+      }
+    )
 
     console.log("Done creating item", doc)
   } catch (err) {
