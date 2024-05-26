df2fd881c93e3ec5e394b3b56ca2a4622b275721
hawraalattar <hanovalattar@gmail.com>
2024-05-26 10:48:10 +0300

    items controller added

--- quickCart/controllers/categories.js
+++ quickCart/controllers/categories.js
@@ -9,9 +9,7 @@ async function index(req, res) {
 async function show(req, res) {
   const paramId = req.params.id
   const categories = await Category.findById(paramId)
-  console.log(paramId)
-  console.log(categories)
-  const items = await Item.find({})
+  const items = await Item.find({ itemType: categories.categoryName })
   res.render('categories/show', {
     title: 'The Category',
     categories,
@@ -19,17 +17,7 @@ async function show(req, res) {
   })
 }
 
-async function showItemDetails(req, res) {
-  console.log('id', req.params.id)
-  const item = await Item.findById(req.params.id)
-  res.render('categories/items/show', {
-    title: 'Item Details',
-    item
-  })
-}
-
 module.exports = {
   index,
-  show,
-  showItemDetails
+  show
 }
