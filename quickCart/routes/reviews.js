ecfbfdd5b6c224ad56e53d517260a2e019cef591
zainabtalaq <zainab.altallaq@gmail.com>
2024-05-27 09:55:44 +0300

    add reviews path in both edit and delete

--- quickCart/routes/reviews.js
+++ quickCart/routes/reviews.js
@@ -4,7 +4,6 @@ const reviewsCtrl = require("../controllers/reviews")
 
 router.post("/items/:id", reviewsCtrl.create)
 router.delete("/items/:id", reviewsCtrl.delete)
-// router.get("/:id/edit", reviewsCtrl.edit)
 router.put("/items/:id", reviewsCtrl.update)
 
 module.exports = router
