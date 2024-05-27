61c871e089078e73e2f40e951316be0e8e275f2d
zainabtalaq <zainab.altallaq@gmail.com>
2024-05-27 13:03:46 +0300

    --

--- quickCart/routes/items.js
+++ quickCart/routes/items.js
@@ -2,5 +2,6 @@ var express = require("express")
 var router = express.Router()
 const itemsCtrl = require("../controllers/items")
 router.get("/items/show/:id", itemsCtrl.show)
+router.get("/items/show/:id", itemsCtrl.addToCart)
 
 module.exports = router
