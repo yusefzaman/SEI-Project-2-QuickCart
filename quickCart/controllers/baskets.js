96534fa1c1f0e1b0a8ca2a1b72ad1d28b7a31f9e
yusefzaman <yusef96@gmail.com>
2024-05-27 21:17:32 +0300

    create basket page and update of css for items detial page

--- quickCart/controllers/baskets.js
+++ quickCart/controllers/baskets.js
@@ -1,2 +1,18 @@
 const Category = require('../models/category')
 const Item = require('../models/item')
+
+async function show(req, res) {
+  const paramId = req.params.id
+  const basket = await Basket.findById(paramId)
+  const items = await Item.find({ itemType: basket.itemName })
+  res.render('basket/index', {
+    title: 'The Basket',
+    basket,
+    items
+  })
+}
+
+
+module.exports = {
+  show
+}
