9a64186eb260f9bfeb722a7c3f97b3559e154955
hawraalattar <hanovalattar@gmail.com>
2024-05-29 10:45:01 +0300

    items can be added to cart

--- quickCart/routes/baskets.js
+++ quickCart/routes/baskets.js
@@ -1,7 +1,8 @@
-var express = require("express")
+var express = require('express')
 var router = express.Router()
-const basketController = require("../controllers/basket")
+const basketController = require('../controllers/basket')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
-router.post("/:id/baskets", basketController.addToCart)
+router.post('/:id/baskets', ensureLoggedIn, basketController.addToCart)
 
 module.exports = router
--- quickCart/routes/categories.js
+++ quickCart/routes/categories.js
@@ -1,9 +1,9 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const categoriesCtrl = require("../controllers/categories")
+const categoriesCtrl = require('../controllers/categories')
 
-router.get("/", categoriesCtrl.index)
-router.get("/about", categoriesCtrl.about)
-router.get("/:id", categoriesCtrl.show)
+router.get('/', categoriesCtrl.index)
+router.get('/about', categoriesCtrl.about)
+router.get('/:id', categoriesCtrl.show)
 
 module.exports = router
