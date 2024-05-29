8a5cd6ecfe535f1c41425d3f7065ce8eb485d097
hawraalattar <hanovalattar@gmail.com>
2024-05-29 14:13:14 +0300

    deleteItem function works

--- quickCart/controllers/categories.js
+++ quickCart/controllers/categories.js
@@ -1,6 +1,5 @@
 const Category = require('../models/category')
 const Item = require('../models/item')
-const mongodb = require('mongodb')
 const Basket = require('../models/basket')
 const User = require('../models/user')
 
@@ -49,7 +48,21 @@ const showBasket = async (req, res) => {
   }
 }
 
+async function deleteItem(req, res) {
+  try {
+    const itemId = req.body.itemId
+    const user = await User.findById(req.user.id).populate('basket')
+    user.basket.items = user.basket.items.filter(
+      (item) => item.toString() !== itemId
+    )
+    await user.basket.save()
+    res.redirect(`/categories/basket/`)
+  } catch (err) {
+    console.log(err)
+  }
+}
 module.exports = {
+  deleteItem,
   showBasket,
   index,
   show,
--- quickCart/routes/categories.js
+++ quickCart/routes/categories.js
@@ -7,5 +7,6 @@ router.get('/', categoriesCtrl.index)
 router.get('/about', categoriesCtrl.about)
 router.get('/basket', ensureLoggedIn, categoriesCtrl.showBasket)
 router.get('/:id', categoriesCtrl.show)
+router.delete('/basket/:id', ensureLoggedIn, categoriesCtrl.deleteItem)
 
 module.exports = router
